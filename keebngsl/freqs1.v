19c4e529885a51393f728692df5e2583b77be1cd
agsb <barcellos.alvaro@gmail.com>
2024-09-18 11:23:26 -0300

    keys for

--- keebngsl/freqs1.v
+++
@@ -1,7 +0,0 @@
-
-# pesos para determinacao de eficiencia
-# using ngsl words
-# t a o e h n d i b f s r y l w u c m p g k v j x q z
-
-# one layer
-1 t a o e h n d i b f r s y l w u c m p g k v j x q z 
